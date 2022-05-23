24ccf2dbd2c1a8b0064017faf89da3d95caa8b27
devshum <57689318+devshum@users.noreply.github.com>
2022-05-23 11:30:33 +0300

    case-insensitive alphabetic characters

--- script.js
+++ script.js
@@ -39,3 +39,15 @@ function findOutlier(integers){
     })
   }
 }
+
+// Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+function duplicateCount(text){
+  const letters = text.toLowerCase().split('')
+  const allDublicates = letters.filter((letter, i) => 
+                                       letters.indexOf(letter) !== i)
+
+  const uniqLetters = new Set([...allDublicates])
+
+  return [...uniqLetters].length
+}
