a5b0a2cf4f8f6e589b4aeab63ddf6ed01077079f
devshum <57689318+devshum@users.noreply.github.com>
2022-05-23 11:10:36 +0300

    Narcissistic number

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('test')
+// Your code must return true or false (not 'true' and 'false') depending upon whether the given number is a Narcissistic number in base 10. This may be True and False in your language, e.g. PHP.
+
+// Error checking for text strings or other invalid inputs is not required, only valid positive non-zero integers will be passed into the function. 
+
+function narcissistic(value) {
+  const allNumbers = `${value}`.split('')
+  const convertedNumbers = []
+
+  const digits = allNumbers.length
+
+  allNumbers.forEach(number => 
+  convertedNumbers.push(number ** digits))
+  
+  const result = convertedNumbers
+  .reduce((curValue, nextValue) => curValue + nextValue)
+  
+  return result === value
+}
